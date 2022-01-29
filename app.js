1b5c15b784a40e71dec728deb573a6c30d7a15a5
Spencer Hulse <hulse.spencer@gmail.com>
2022-01-29 01:32:42 -0500

    added the logic to generate a webpage

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+/* const name = profileDataArgs[0];
+const github = profileDataArgs[1]; */
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile("index.html", generatePage(name, github), (err) => {
+  if (err) throw err;
+
+  console.log(
+    "Portfolio complete! Check out the index.html to see the output!"
+  );
+});
